03ef1746d6297ebad62a44b6ee8615f21cf6da1d
rwbykz <dreamervt8@gmail.com>
2020-09-22 22:17:41 +0600

    e-orda test task

---
+++ projects/e-orda/js/common.js
@@ -0,0 +1,16 @@
+// Scroll with jQuery
+// $(document).ready(function () {
+//   $('.scroll-anch').click(function () {
+//     var scroll_el = $(this).data('target');
+//     if ($(scroll_el).length != 0) {
+//       $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 750);
+//     }
+//     return false;
+//   });
+
+//   $('.scroll-anch').click(function () {
+//     $('html body').animate({
+//       scrollTop: $(this).offset().top
+//     }, 750)
+//   });
+// });
