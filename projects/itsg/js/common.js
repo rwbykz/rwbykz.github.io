42e51c16294063ad5273060c45b0c0614ad28de0
rwbykz <dreamervt8@gmail.com>
2020-10-11 14:22:50 +0600

    itsg update

--- projects/itsg/js/common.js
+++ projects/itsg/js/common.js
@@ -1,46 +1,46 @@
 //Smooth scroll
 // console.log('start position = ' + window.pageYOffset);
-const smoothScroll = (target, duration) => {
-  var target = document.querySelector(target);
-  let targetPosition = target.getBoundingClientRect().top;
-  let startPosition = window.pageYOffset;
-  let distance = targetPosition - startPosition;
-  let startTime = null;
+// const smoothScroll = (target, duration) => {
+//   var target = document.querySelector(target);
+//   let targetPosition = target.getBoundingClientRect().top;
+//   let startPosition = window.pageYOffset;
+//   let distance = targetPosition - startPosition;
+//   let startTime = null;
 
-  console.log('start position = ' + startPosition);
-  console.log('target position = ' + targetPosition);
+//   console.log('start position = ' + startPosition);
+//   console.log('target position = ' + targetPosition);
 
-  const animation = currentTime => {
-    if (startTime === null) startTime = currentTime;
-    // console.log(startTime);
-    let timeElapsed = currentTime - startTime;
-    // console.log('elapsed ' + timeElapsed);
-    let run = ease(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run);
+//   const animation = currentTime => {
+//     if (startTime === null) startTime = currentTime;
+//     // console.log(startTime);
+//     let timeElapsed = currentTime - startTime;
+//     // console.log('elapsed ' + timeElapsed);
+//     let run = ease(timeElapsed, startPosition, distance, duration);
+//     window.scrollTo(0, run);
 
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
-  // Больше плавных функций здесь ---> http://www.gizma.com/easing/
-  const ease = (t, b, c, d) => {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t * t + b;
-    t -= 2;
-    return c / 2 * (t * t * t + 2) + b;
-  }
+//     if (timeElapsed < duration) requestAnimationFrame(animation);
+//   }
+//   // Больше плавных функций здесь ---> http://www.gizma.com/easing/
+//   const ease = (t, b, c, d) => {
+//     t /= d / 2;
+//     if (t < 1) return c / 2 * t * t * t + b;
+//     t -= 2;
+//     return c / 2 * (t * t * t + 2) + b;
+//   }
 
-  requestAnimationFrame(animation);
-}
+//   requestAnimationFrame(animation);
+// }
 
-let heroBtn = document.querySelector('.hero__btn');
-let heroToServices = document.querySelector('.hero-services-link');
+// let heroBtn = document.querySelector('.hero__btn');
+// let heroToServices = document.querySelector('.hero-services-link');
 
-heroBtn.addEventListener('click', () => {
-  smoothScroll('.about', 2350);
-});
+// heroBtn.addEventListener('click', () => {
+//   smoothScroll('.about', 2350);
+// });
 
-heroToServices.addEventListener('click', () => {
-  smoothScroll('.services', 2350);
-});
+// heroToServices.addEventListener('click', () => {
+//   smoothScroll('.services', 2350);
+// });
 
 //swiper slider
 
@@ -68,18 +68,20 @@ let mySwiper = new Swiper(slider, {
 })
 
 // Scroll with jQuery
-// $(document).ready(function () {
-//   $('.scroll-anch').click(function () {
-//     var scroll_el = $(this).data('target');
-//     if ($(scroll_el).length != 0) {
-//       $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 750);
-//     }
-//     return false;
-//   });
+$(document).ready(function () {
+  $('.scroll-anch').click(function () {
+    console.log('hello');
+    var scroll_el = $(this).data('target');
+    if ($(scroll_el).length != 0) {
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1750);
+    }
+    return false;
+  });
 
-//   $('.scroll-anch').click(function () {
-//     $('html body').animate({
-//       scrollTop: $(this).offset().top
-//     }, 750)
-//   });
-// });
+  $('.scroll-anch').click(function () {
+    console.log('hello');
+    $('html body').animate({
+      scrollTop: $(this).offset().top
+    }, 1750)
+  });
+});
