f5d95267178968a610c730f7d6a5920320880e56
rwbykz <dreamervt8@gmail.com>
2021-01-30 19:52:02 +0600

    simpleweb added

---
+++ projects/simpleweb/js/common.js
@@ -0,0 +1,15 @@
+//Form validation
+
+const nameField = document.querySelector('input[name="user_name"]')
+// const phoneField = document.getElementById("phone")
+const btnForm = document.querySelector('.form__btn')
+
+// document.querySelector('.form__btn').addEventListener('click', function helpCode() {
+//   if (nameField === null) {
+//     document.getElementById("name").classList.add("form__error")
+//   } else {
+//     document.getElementById("name").classList.remove("form__error")
+//   }
+// })
+
+
---
+++ projects/simpleweb/js/smoothScroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.scroll-anch').click(function () {
+    var scroll_el = $(this).data('target');
+    if ($(scroll_el).length != 0) {
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 750);
+    }
+    return false;
+  });
+
+  $('.scroll-anch').click(function () {
+    $('html body').animate({
+      scrollTop: $(this).offset().top
+    }, 750)
+  });
+});
